2be74b4565884c50b27a655ad9d0bafd53e6dac0
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2022-10-03 21:47:29 -0500

    Exc3

---
+++ Exc3/src/test/java/com/exc3/Exc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exc3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
